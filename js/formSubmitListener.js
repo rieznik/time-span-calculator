8672297daf5dbbb9fb56edb428e82d588f001c7b
Kateryna Rieznik <rieznik.k@gmail.com>
2023-04-09 16:44:28 +0300

    get inputted data

--- js/formSubmitListener.js
+++ js/formSubmitListener.js
@@ -1,11 +1,22 @@
-const formElement = document.querySelector('form');
+const formElement = document.getElementById('calc-form');
+const startDateElem = document.getElementById('start-date');
+const endDateElem = document.getElementById('end-date');
+const daysSelection = document.getElementById('filter-days-selection');
+const unitsElements = document.querySelectorAll('fieldset [type="radio"]');
+
+const getUnits = (elsSet) => {
+  const checked = Array.from(elsSet).filter((radio) => radio.checked);
+  return checked[0].getAttribute('id');
+};
 
 export const formSubmitListener = (cb) => {
   formElement.addEventListener('submit', (event) => {
     event.preventDefault();
     const inputData = {
-      startDate: new Date(),
-      daysSelection: 'all',
+      startDate: new Date(startDateElem.value),
+      endDate: new Date(endDateElem.value),
+      daysSelection: daysSelection.value,
+      units: getUnits(unitsElements),
     };
     cb(inputData);
   });

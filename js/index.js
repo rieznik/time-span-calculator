716b40bee73fbdddbbebe060893a9e941a08fe74
Kateryna Rieznik <rieznik.k@gmail.com>
2023-04-10 12:50:33 +0300

    decouple updateLog

---
+++ js/index.js
@@ -0,0 +1,23 @@
+import { formSubmitListener } from './formSubmitListener.js';
+import { showResult } from './showResult.js';
+import { calculateDuration } from './calculateDuration.js';
+import { getActivityLog, setActivityLog } from './ls.js';
+import { renderActivityLog } from './renderActivityLog.js';
+
+const startApp = () => {
+  formSubmitListener((inputData) => {
+    const result = calculateDuration(inputData);
+    showResult(result, inputData.units);
+
+    const logEntry = {
+      result,
+      units: inputData.units,
+      startDate: inputData.startDate,
+      endDate: inputData.endDate,
+    };
+    setActivityLog(logEntry);
+    renderActivityLog(getActivityLog());
+  });
+};
+
+document.addEventListener('DOMContentLoaded', startApp);
